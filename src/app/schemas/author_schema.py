c650625c4a83abdcf9e07650d78a080b088907ec
NeverCry <dima_panchenko_2222@vk.com>
2025-05-13 21:26:07 +0500

    Fixed Author schema

--- src/app/schemas/author_schema.py
+++ src/app/schemas/author_schema.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, EmailStr
 
 class AuthorSchema(BaseModel):
-	name: str = Field(ge=2, le=50)
+	name: str = Field(min_length=2, max_length=50)
 	age: int = Field(ge=12, le=120)
 	email: EmailStr
 
